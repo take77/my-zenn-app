a718c7cdd9907cd30c5630c59daa0b34c35dc710
take77 <take77.tech@gmail.com>
2023-01-23 17:22:49 +0900

    done step 01

---
+++ frontend/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Zenn-app
+      </h1>
+      <h2 class="subtitle">
+        {{ subTitle }}
+      </h2>
+      <button @click="getSomething">
+        タスク取得
+      </button>
+      <ul v-for="task in tasks" :key="task.id">
+        <li style="text-align: left;">{{ task.title }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subTitle: 'Zenn is good service!!',
+      tasks: []
+    }
+  },
+  methods: {
+    async getSomething() {
+      const response = await this.$axios.$get('http://localhost:3001/api/v1/tasks')
+      this.tasks = JSON.parse(response.tasks)
+    }
+  },
+}
+</script>
